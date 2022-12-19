6e87a59e156a3ec7c2e06b38c187ab7ca05715f6
RifadhRFD <rifadhrfd@gmail.com>
2022-12-19 16:46:39 +0530

    useState complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import './App.css';
-import Employee from './component/Employee';
+import Employee from './component/Employee'
+import { useState } from 'react'
 
 function App() {
 
   const showEmployee = true;
+  const [role, setRole] = useState('dev');
 
   return (
     <div className="App">
       <header className="App-header">
-        {showEmployee ?  
+        {showEmployee ?  (
         <>
+          <input type='text' onChange={(e) => {
+            console.log(e.target.value)
+            setRole(e.target.value)
+          }}
+          />
+
           <Employee name="Mohammed" role="Intern"/>
           <Employee name="Rifadh" role="Software Engineer"/>
-          <Employee name="Rijan" role="CEO"/>
+          <Employee name="Rijan" role={role}/>
           <Employee name="Rijan" />
         </>
-        :
+        ):(
           <p>You cannot see me.</p>
-      }
+          )}
       </header>
     </div>
   );
