d580fb3d2f0cef1f16856c228e306d8d51312087
RifadhRFD <rifadhrfd@gmail.com>
2022-12-19 22:42:04 +0530

    data

--- src/App.js
+++ src/App.js
@@ -1,32 +1,72 @@
 import './index.css';
 import Employee from './component/Employee'
 import { useState } from 'react'
+import {v4 as uuidv4} from 'uuid'
 
 function App() {
 
   const showEmployee = true;
-  const [role, setRole] = useState('dev');
+  const [role, setRole] = useState('Developer')
+  const [employees, setEmployees] = useState(
+    [
+      {
+        name: 'Rifadh',
+        role: 'Software'
+      },
+      {
+        name: 'Bobby',
+        role: 'Developer'
+      },
+      {
+        name: 'Cane',
+        role: 'Software'
+      },
+      {
+        name: 'Steve',
+        role: 'Developer'
+      },
+      {
+        name: 'Marvel',
+        role: 'Software'
+      },
+      {
+        name: 'Captain',
+        role: 'Developer'
+      },
+      {
+        name: 'Robert',
+        role: 'Software'
+      },
+      {
+        name: 'Lilly',
+        role: 'Developer'
+      }
+    ]
+  )
 
   return (
-    <div className="App bg-red-300">
-      <header className="App-header">
+    <div className="App">
         {showEmployee ?  (
         <>
-          <input type='text' onChange={(e) => {
-            console.log(e.target.value)
-            setRole(e.target.value)
+        <input type='text' onChange={(e) => {
+            console.log(e.target.value);
+            setRole(e.target.value);
           }}
           />
+          
+        <div className="flex flex-wrap justify-center">
 
-          <Employee name="Mohammed" role="Intern"/>
-          <Employee name="Rifadh" role="Software Engineer"/>
-          <Employee name="Rijan" role={role}/>
-          <Employee name="Rijan" />
+          {employees.map((employee) => {
+            console.log(employee);
+            return(
+            <Employee key={uuidv4()} name={employee.name} role={employee.role} />
+            );
+          })}
+        </div>
         </>
         ):(
           <p>You cannot see me.</p>
           )}
-      </header>
     </div>
   );
 }
